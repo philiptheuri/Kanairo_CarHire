d8d459760ab2c371bdea5e60ad458a64cbfabc86
PJ Theuri <pjtheuri@gmail.com>
2021-10-07 02:55:03 +0300

    Imports

--- django_apps/carhire/carhire/urls.py
+++ django_apps/carhire/carhire/urls.py
@@ -13,9 +13,23 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
+from django.views.generic import RedirectView
+from django.urls import include
 from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+urlpatterns += [
+    path ('carlisting/', include ('carlisting.urls')),
+]
+
+urlpatterns += [
+    path('' , RedirectView.as_view(url='carlisting/', permanent=True)),
+]
+
+urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
